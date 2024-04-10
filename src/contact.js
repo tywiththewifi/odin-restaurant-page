895524a2c2e2ae1f27fa26f9615c590a52e9dee3
tywiththewifi <156716969+tywiththewifi@users.noreply.github.com>
2024-04-10 15:51:05 -0700

    Debugged and cleaned up contact.js to make site functional

--- src/contact.js
+++ src/contact.js
@@ -1,18 +1,11 @@
 import { newHeader, newParagraph } from "./pageBuilder";
-import { displayPage } from "./index.js";
 
 function buildContact() {
     newHeader('Conatct Us');
     newParagraph('For inquiries or reservations, please do not reach out to us.');
     newParagraph('If you must insist, you can find us at:');
     newParagraph('________________');
-}
-
-const contactButton = document.getElementById('conact_button');
-
-contactButton.addEventListener('click', function() {
-    displayPage(buildContact);
-});
+};
 
 // 
 
--- src/index.js
+++ src/index.js
@@ -41,7 +41,3 @@ function clearPage() {
 }
 
 buildHome();
-
-export {
-    displayPage
-}
